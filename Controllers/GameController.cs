8471dfd037a53af1e4963bdba65a000fa4155488
Peter Hafid <peter@ctrl-c.liu.se>
2017-07-22 17:55:17 +0200

    First controller

---
+++ Controllers/GameController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System;
+using GameReviewer.Models;
+
+namespace GameReviewer.Controllers
+{
+    public class GameController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View(Game.ReadAll());
+        }
+
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Game game)
+        {
+            Game.Create(game);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ GlobalVariables.cs
@@ -0,0 +1,7 @@
+﻿namespace GameReviewer
+{
+    public class GlobalVariables
+    {
+        
+    }
+}
