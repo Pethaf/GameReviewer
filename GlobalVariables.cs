8c2da3278faa4ba91d4b8c2e4667ea2653a95762
Peter Hafid <peter@ctrl-c.liu.se>
2017-07-22 18:07:32 +0200

    Add file to handle global variables

--- GlobalVariables.cs
+++ GlobalVariables.cs
@@ -1,7 +1,13 @@
-﻿namespace GameReviewer
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GameReviewer.Models;
+
+namespace GameReviewer
 {
     public class GlobalVariables
     {
-        
+        public static List<Game> Games { get; set; } = new List<Game>();
     }
 }
